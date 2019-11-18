aefe72305e6550574926b178d6d95a543581d820
pphhss <gustjs2080@naver.com>
2019-11-18 18:07:02 +0900

    [라즈베리파이] MQTTAdater sendData기능 구현 완료

--- rpi/mqttAdapter.py
+++ rpi/mqttAdapter.py
@@ -10,6 +10,7 @@ class MQTTAdapter():
 
     def __init(self):
         def __callback(_dict):
+            # type : true/false // idx : integer
             self.__onMessage(_dict['type'], _dict['idx'])
         self.mqtt.subscribe(config.MQTT['topic_device'], 1, __callback)
 
@@ -19,12 +20,26 @@ class MQTTAdapter():
     def close(self):
         self.mqtt.disconnect()
 
+    def sendData(self, _idx, _power, _sound):
+        # 데이터 포맷맞추기 필요.
+        self.mqtt.publish(config.MQTT['topic_sheet_log'], {
+                          "sheet_use_idx": _idx, "power": _power, "sound": _sound}, 1)
+
 
 if __name__ == "__main__":
+    '''
     def testCallback(_type, _idx):
         print("type : "+str(_type)+" idx : "+str(_idx))
     ma = MQTTAdapter(testCallback)
     for i in range(20000000):
         pass
 
+    ma.close()
+    '''
+
+    def testCallback(_type, _idx):
+        print("type : "+str(_type)+" idx : "+str(_idx))
+    ma = MQTTAdapter(testCallback)
+
+    ma.sendData(10, 51, 50)
     ma.close()
